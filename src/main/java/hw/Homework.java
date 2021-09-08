e0c140c8b55fed62d241fbb7bc0a23e645e84de4
Shafi Rafiqi <rafiqishafi@gmail.com>
2021-09-07 23:13:42 -0400

    first commit

---
+++ src/main/java/hw/Homework.java
@@ -0,0 +1,51 @@
+package hw;
+
+public class Homework {
+    public static void main(String[] args) {
+        //Java Get Started
+        System.out.println("Coding With Intellij");
+        //Java Syntax
+        System.out.println("Hello World");
+        //Java Variables
+        String name = "Shafi";
+        System.out.println(name);
+        //Java DataTypes
+        int myNum = 768;
+        System.out.println(myNum);
+        short numbers = 24437;
+        System.out.println(numbers);
+        //Java Typecasting 1 widening casting example and 1 narrowing casting example
+        short myShor = 12111;
+        long myLon = myShor;
+        System.out.println(myShor);
+        System.out.println(myLon);
+        double myChar = 14.8;
+        char myDbl = (char) myChar;
+        System.out.println(myChar);
+        System.out.println(myDbl);
+        //Java Operators
+        int num1 = 100 + 71;
+        System.out.println(num1);
+        //Java strings example of strings and string concatenation
+        String myCity = "Queens";
+        System.out.println(myCity);
+        String iLike = "I like to eat apples and bananas,";
+        String iLike2 = "I also like to eat steak.";
+        System.out.println(iLike + " " + iLike2);
+        //Java math
+        int randomNum = (int)(Math.random() * 24897);
+        System.out.println(randomNum);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
